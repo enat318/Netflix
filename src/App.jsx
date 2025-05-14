689ec1aa62601a0ec166e721a532a2d0015f9a07
enat318 <assefadagim1@gmail.com>
2025-05-14 20:50:55 +0300

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
-
+import Home from './pages/Home/Home'
 function App() {
   
 
   return (
     <>
-      
+      <Home/>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import "./Header.css"
+import logo from "../../assets/netflix.png"
+import { IoSearch } from "react-icons/io5";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { MdOutlineAccountBox } from "react-icons/md";
+import { IoMdArrowDropdown } from "react-icons/io";
+function Header() {
+  return (
+    <div>
+      <div className="header-outer-container">
+        <div className="header-container">
+          <div className="header-left">
+            <ul>
+                <li>
+                    <img src={logo}alt="netflix logo" />
+                </li>
+              <li>Netflix</li>
+              <li>Home</li>
+              <li>Tv Show</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>My List</li>
+              <li>Browse by Language</li>
+            </ul>
+          </div>
+          <div className="header-right">
+            <ul>
+              <li>
+                <IoSearch />
+              </li>
+              <li>
+                <IoMdNotificationsOutline />
+              </li>
+              <li>
+                <MdOutlineAccountBox />
+              </li>
+              <li>
+                <IoMdArrowDropdown />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header
