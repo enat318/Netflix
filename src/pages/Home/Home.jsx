dd22074e2ee41387896c9ffa295568ae4d0b2545
enat318 <assefadagim1@gmail.com>
2025-05-18 12:07:26 +0300

    Rows

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/Banner/Banner'
+import RowList from '../../components/Rows/RowList/RowList'
+
 
 function Home() {
   return (
     <div>
         <Header/>
-        <Footer/>
+        <Banner/>
+        <RowList/>
+       <Footer/>
     </div>
   )
 }
